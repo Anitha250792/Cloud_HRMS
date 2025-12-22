f1c70e6a8e50ecc3b0d280ffbc374ce828c142f1
Anitha250792 <ntanithasaravanan@gmail.com>
2025-12-22 07:24:21 +0530

    Fix employees urls import error

--- backend/employees/urls.py
+++ backend/employees/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path
-from .views import (
-    list_employees,
-    create_employee,
-    delete_employee,
-    update_employee,
-)
+from . import views   # ✅ IMPORTANT
 
 urlpatterns = [
-    path("", list_employees),
-    path("create/", create_employee),
-    path("<int:pk>/", views.get_employee),  
-    path("update/<int:pk>/", update_employee),   # ✅ ADD THIS
-    path("delete/<int:pk>/", delete_employee),
+    path("", views.list_employees),
+    path("create/", views.create_employee),
+    path("<int:pk>/", views.get_employee),
+    path("update/<int:pk>/", views.update_employee),
+    path("delete/<int:pk>/", views.delete_employee),
 ]
