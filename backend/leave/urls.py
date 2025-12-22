deee982a9e55be2cd4574a02808d2ada5e108ede
Anitha250792 <ntanithasaravanan@gmail.com>
2025-12-22 16:34:41 +0530

    leaves/urls

--- backend/leave/urls.py
+++ backend/leave/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LeaveViewSet
+from .views import LeaveViewSet, my_leaves
+from django.urls import path
 
 router = DefaultRouter()
 router.register("leave", LeaveViewSet, basename="leave")
 
-urlpatterns = [
-    path("", include(router.urls)),
+urlpatterns = router.urls + [
+    path("leave/my/", my_leaves),
 ]
