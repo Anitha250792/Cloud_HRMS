29a82d92239df3d8bdfeacbf4ebfd0ac71261025
Anitha250792 <ntanithasaravanan@gmail.com>
2026-01-08 17:00:09 +0530

    Fix Attendance MyLeaves

--- backend/attendance/urls.py
+++ backend/attendance/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import AttendanceViewSet, my_today_attendance
+from .views import (
+    AttendanceViewSet,
+    my_today_attendance,
+    attendance_records,
+    attendance_balance
+)
 
 urlpatterns = [
     path("check-in/", AttendanceViewSet.as_view({"post": "check_in"})),
     path("check-out/", AttendanceViewSet.as_view({"post": "check_out"})),
     path("my-today/", my_today_attendance),
+    path("records/", attendance_records),
+    path("balance/", attendance_balance),
 ]
