ded5781a577a7a3b018adc110e8e40c949b136d5
Anitha250792 <ntanithasaravanan@gmail.com>
2026-06-25 17:20:48 +0530

    Fix auth

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,38 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth import login
-
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from .models import User
 from .serializers import RegisterSerializer, LoginSerializer
 
-
-# =====================================================
-# REGISTER VIEW
-# =====================================================
-
-class RegisterView(APIView):
-    authentication_classes = []
-    permission_classes = []
-
-    def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
-
-        if serializer.is_valid():
-            user = serializer.save()
-            return Response(
-                {"message": "Registration successful"},
-                status=status.HTTP_201_CREATED,
-            )
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# =====================================================
-# LOGIN VIEW
-# =====================================================
 
 class LoginView(APIView):
     authentication_classes = []
@@ -43,6 +15,7 @@ class LoginView(APIView):
         serializer = LoginSerializer(data=request.data)
 
         if not serializer.is_valid():
+
             return Response(
                 serializer.errors,
                 status=status.HTTP_401_UNAUTHORIZED,
@@ -52,10 +25,10 @@ class LoginView(APIView):
 
         refresh = RefreshToken.for_user(user)
 
-        return Response(
+        access_token = str(refresh.access_token)
+
+        response = Response(
             {
-                "access": str(refresh.access_token),
-                "refresh": str(refresh),
                 "role": user.role,
                 "user": {
                     "id": user.id,
@@ -68,12 +41,13 @@ class LoginView(APIView):
         )
 
         response.set_cookie(
-        key="access_token",
-        value=access_token,
-        httponly=True,
-        secure=True,
-        samesite="None",
-        max_age=3600,
+            key="access_token",
+            value=access_token,
+            httponly=True,
+            secure=True,
+            samesite="None",
+            max_age=3600,
+            path="/",
         )
 
         return response
